df3f5dae3305e6a2c97abb20d0935e24f401ec38
Meduza3 <jellyfish200325@gmail.com>
2023-10-19 19:22:53 +0200

    Test

---
+++ sem3/tp/Lista1/my-app/src/test/java/com/example/AppTest.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    public void testKwadrat() {
+        assertEquals(App.kwadrat(4.0), 16.0);
+        assertEquals(App.kwadrat(5.0), 25.0);
+        assertEquals(App.kwadrat(-5.0), 25.0);
+    }
+}
