659fb67ae212b069f4839d5fbac5551812c047dd
Meduza3 <jellyfish200325@gmail.com>
2024-04-13 16:27:03 +0100

    Added Rust solution

--- sem4/jpp/l2/GF1234577.java
+++ sem4/jpp/l2/GF1234577.java
@@ -2,6 +2,27 @@ public class GF1234577 {
     private int value;
     private int characteristic = 1234577;
 
+    private int modInverse(int a, int mod){
+        int m0 = mod, t, q;
+        int x0 = 0, x1 = 1;
+        if(mod == 1) {
+            return 0;
+        }
+        while(a > 1) {
+            q = a / mod;
+            t = mod;
+            mod = a % mod;
+            a = t;
+            t = x0;
+            x0 = x1 - q * x0;
+            x1 = t;
+        }
+        if (x1 < 0){
+            x1 += m0;
+        }
+        return x1;
+    }
+
     public int getValue() {
         return this.value;
     }
@@ -29,28 +50,45 @@ public class GF1234577 {
     public GF1234577 sub(GF1234577 rhs) {
         int res = value - rhs.getValue();
         if (res < 0 ) {
-            return GF1234577(characteristic + res);
+            return new GF1234577(characteristic + res);
         } else {
-            return GF1234577(res);
+            return new GF1234577(res);
         }
     }
 
     public GF1234577 mult(GF1234577 rhs) {
-        return GF1234577(value * rhs.getValue());
+        return new GF1234577(value * rhs.getValue());
     }
 
     public GF1234577 div(GF1234577 rhs) {
         if(rhs.getValue() == 0){
-            throw IllegalArgumentException("Can't divide by zero!");
+            throw new IllegalArgumentException("Can't divide by zero!");
         }
 
         int inverse = modInverse(rhs.getValue(), rhs.getCharacteristic());
+        return new GF1234577(value * inverse);
+    }
 
-        
+    public Boolean eq(GF1234577 rhs) {
+        return value == rhs.getValue();
     }
 
     @Override
     public String toString() {
-        return Integer.parseInt(value);
+        return Integer.toString(value);
+    }
+
+    public static void main(String[] args) {
+        GF1234577 a = new GF1234577(4591);
+        GF1234577 b = new GF1234577(1435);
+        GF1234577 c = new GF1234577(5925);
+        GF1234577 d = new GF1234577(14854);
+
+        System.out.println("a + b = " + a.add(b));
+        System.out.println("a - d = " + a.sub(d));
+        System.out.println("d * c = " + d.mult(c));
+        System.out.println("c / b = " + c.sub(b));
+        System.out.println("is a equal to b*d? " + (a.eq(b.mult(d)) ? "Yes" : "No"));
+        
     }
 }
