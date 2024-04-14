d20fe9127bf6648259093235e84630dc76540f60
Meduza3 <jellyfish200325@gmail.com>
2024-04-14 12:17:23 +0100

    Working protocol

--- sem4/jpp/l3/DHSetup.cpp
+++ sem4/jpp/l3/DHSetup.cpp
@@ -1,13 +1,13 @@
-template<typename T>
+#include "DHSetup.h"
+
+template<int CHARACTERISTIC>
 class DHSetup {
 
     private:
-        GF_Int<T> field;
-        T characteristic = 1234577;
-        T generator = 0;
+        GF_Int<CHARACTERISTIC> generator;
 
         void generateGenerator(){
-            
+            generator = GF_Int<CHARACTERISTIC>(3);
         }
 
     public:
@@ -16,17 +16,29 @@ class DHSetup {
         generateGenerator();
     }
 
-    T power(T a, unsigned long b) {
-        unsigned long result = 1;
+    GF_Int<CHARACTERISTIC> power(GF_Int<CHARACTERISTIC> a, unsigned long b) {
+        GF_Int<CHARACTERISTIC> result(1);
 
         for(int i = 0; i < b; i++){
-            result = result * a % characteristic;
+            result = result * a;
         }
 
-        return static_cast<T>(result);
+        return result;
+    }
+
+    GF_Int<CHARACTERISTIC> getGenerator() {
+        return generator;
     }
 };
 
 
 int main() {
+    DHSetup<1234577> dhSetup;
+    GF_Int<1234577> generator = dhSetup.getGenerator();
+    GF_Int<1234577> powerResult = dhSetup.power(generator, 5);
+
+    std::cout << "Generator: " << generator.get_value() << std::endl;
+    std::cout << "Generator^5: " << powerResult.get_value() << std::endl;
+
+    return 0;
 }
