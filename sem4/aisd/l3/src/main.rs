24be978a9bf16c3b4fb1c1da0ec8a975d84d7a01
Meduza3 <jellyfish200325@gmail.com>
2024-04-28 21:02:40 +0200

    Refactor main.rs to handle command line arguments and input from stdin

--- sem4/aisd/l3/src/main.rs
+++ sem4/aisd/l3/src/main.rs
@@ -1,25 +1,54 @@
+use std::io::{self, BufRead};
+
 mod select;
 mod randselect;
 
 fn main() {
-    println!("Hello, LISTA 3!");
-    let mut arr = vec![20, 54, 83, 290, 321, 543, 180, 320, 854, 19, 8, 123, 342, 901];
-    let arr_len = arr.len();
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("🦀: {} run <-rand|-select>", args[0]); // Use standard error for error messages
+        std::process::exit(1); // Exit with a non-zero status code indicating failure
+    }
 
+    let mode = match args[1].as_str() {
+        "-rand" => "rand",
+        "-select" => "select",
+        _ => {
+            eprintln!("🦀: {} run <-rand|-select>", args[0]); // Uniform error handling
+            std::process::exit(1);
+        }
+    };
 
-    println!("{:?}", &arr);
-    println!("{:?}", randselect::rand_select(&mut arr, 0, arr_len - 1, 1));
-    println!("{:?}", randselect::rand_select(&mut arr, 0, arr_len - 1, 2));
-    println!("{:?}", randselect::rand_select(&mut arr, 0, arr_len - 1, 3));
-    println!("{:?}", randselect::rand_select(&mut arr, 0, arr_len - 1, 4));
-    println!("{:?}", randselect::rand_select(&mut arr, 0, arr_len - 1, 5));
-    
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let input = line.unwrap();
+        let numbers: Vec<usize> = input
+            .split_whitespace()
+            .map(|num| num.parse().unwrap())
+            .collect();
 
-    println!("{:?}", select::select(&mut arr, 0, arr_len - 1, 1));
-    println!("{:?}", select::select(&mut arr, 0, arr_len - 1, 2));
-    println!("{:?}", select::select(&mut arr, 0, arr_len - 1, 3));
-    println!("{:?}", select::select(&mut arr, 0, arr_len - 1, 4));
-    println!("{:?}", select::select(&mut arr, 0, arr_len - 1, 5));
+            if numbers.len() < 3 {
+                eprintln!("🦀: Zapomniałeś czegoś w tablicy");
+                continue;
+            }
 
+        let size = numbers[0];
+        let i = numbers[1];
+        let mut numbers: Vec<usize> = numbers[2..].to_vec();
+        let initial_numbers = numbers.clone();
+        let goal = if mode == "rand" {
+            randselect::rand_select(&mut numbers, 0, size - 1, i)
+        } else {
+            select::select(&mut numbers, 0, size - 1, i)
+        };
+        if size <= 50 {
+            println!("Tryb: {mode}");
+            println!("Przed SELECT: {:?}", initial_numbers);
+            println!("Po SELECT: {:?}", numbers);
+            println!("{i}-ta statystyka pozycyjna: {goal}");
+            numbers.sort();
+            println!("Posortowany: {:?}", numbers);
+        }
+    }
 }
 
