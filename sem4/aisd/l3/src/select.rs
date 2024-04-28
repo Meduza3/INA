412c987127dcd3ef0b2e0259265bc8f5240bbc47
Meduza3 <jellyfish200325@gmail.com>
2024-04-28 20:18:18 +0200

    Implemented select function in select.rs

--- sem4/aisd/l3/src/select.rs
+++ sem4/aisd/l3/src/select.rs
@@ -1,5 +1,51 @@
+pub fn select(arr: &mut [usize], p: usize, q:usize, i:usize) -> usize {
+    if p == q {
+        return arr[p];
+    }
 
-pub fn select(a: Vec<i32>, p: i32, q: i32, i: i32 ) {
-    // Podziel A[p..q] na 5 elementowe podtablice
-    // Znajdz me
+    let pivot_index = partition_median_of_medians(arr, p, q);
+    let k = pivot_index - p + 1;
+
+    if i == k {
+        arr[pivot_index]
+    } else if i < k {
+        select(arr, p, pivot_index - 1, i)
+    } else {
+        select(arr, pivot_index + 1, q, i - k)
+    }
+
+}
+
+fn partition_median_of_medians(arr: &mut [usize], p: usize, q: usize) -> usize {
+    let mut medians = Vec::new();
+    let mut start = p;
+    while start <= q {
+        let end = std::cmp::min(start + 4, q);
+        let median = median_of_five(&mut arr[start..=end]);
+        medians.push(median);
+        start += 5;
+    }
+
+    let medians_len = medians.len();
+    let median_of_medians = if medians.len() <= 5 {
+        median_of_five(&mut medians) 
+    } else {
+        select(&mut medians, 0, medians_len - 1, medians_len / 2)
+    };
+
+    let mut i = p;
+    let mut j = q;
+    while i < j {
+        while arr[i] < median_of_medians { i += 1; }
+        while arr[j] > median_of_medians { j -= 1; }
+        if i < j {
+            arr.swap(i, j);
+        }
+    }
+    i
+}
+
+fn median_of_five(arr: &mut [usize]) -> usize {
+    arr.sort();
+    arr[arr.len() / 2]
 }
