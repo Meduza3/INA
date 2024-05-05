8765f29fdd17675aec8aa2c6d2bb2730ea71d9a8
Meduza3 <jellyfish200325@gmail.com>
2024-05-05 18:54:17 +0200

    Refactor code to include plotting of data and running experiments

---
+++ sem4/aisd/l3/plot_data.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+
+x_values = []
+y_values = []
+
+# Read data from the file
+with open('output.txt', 'r') as file:
+    for line in file:
+        x, y, z = line.split()  # Assuming the values are separated by whitespace
+        x_values.append(float(x))
+        y_values.append(float(y))
+
+# Calculate the average of the corresponding y values
+averages = {}
+for x, y in zip(x_values, y_values):
+    if x not in averages:
+        averages[x] = []
+    averages[x].append(y)
+
+x_avg = []
+y_avg = []
+for x, y_list in averages.items():
+    x_avg.append(x)
+    y_avg.append(sum(y_list) / len(y_list))
+
+# Plot the averages
+plt.plot(x_avg, y_avg)
+plt.xlabel('Rozmiar tablicy')
+plt.ylabel('Średnia ilość porównań')
+plt.title('Porównania - Rand Select')
+plt.savefig('comparisons_rand.png')
