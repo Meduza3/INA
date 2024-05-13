f7eec6a6db280eef3c6471c5851c67ea12d32030
Meduza3 <jellyfish200325@gmail.com>
2024-05-13 20:31:21 +0200

    Progress

--- sem4/ts/ramki2/main.go
+++ sem4/ts/ramki2/main.go
@@ -9,21 +9,41 @@ func main() {
 
 	//Read file Z
 
-	z_contents, err := os.ReadFile("z")
-	check(err)
+	z_contents, _ := os.ReadFile("z")
 
 	z_contents_string := string(z_contents)
 	fmt.Println(z_contents_string)
+	header := "_____header_____"
+	crc := "______crc_______"
 
-	// Open file W with write mode and create if it doesn't exist
-
+	final_string := "01111110"
+	final_string += header
+	//Bit stuffing - Dodaj 0 po pięciu jedynkach na raz
+	final_string += stuff_bits(z_contents_string)
+	final_string += crc
+	final_string += "01111110"
 	file, _ := os.OpenFile("w", os.O_WRONLY|os.O_CREATE, 0644)
 	defer file.Close()
-	for _, signal_value := range z_contents_string {
-		_, err := file.WriteString(fmt.Sprintf("%c", signal_value))
-		check(err)
+	_, err := file.WriteString(final_string)
+	check(err)
+}
+
+// Print to file W
+
+func stuff_bits(signal string) string {
+	stuffed_signal := ""
+	consecutive_ones := 0
+	for _, signal_value := range signal {
+		stuffed_signal = stuffed_signal + string(signal_value)
+		if string(signal_value) == "1" {
+			consecutive_ones++
+		}
+		if consecutive_ones == 5 {
+			consecutive_ones = 0
+			stuffed_signal = stuffed_signal + "0"
+		}
 	}
-	// Print to file W
+	return stuffed_signal
 }
 
 func check(e error) {
