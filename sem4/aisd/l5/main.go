a1b2f03b270ca0cd9482ce39ae41691dc30cb1bf
Meduza3 <jellyfish200325@gmail.com>
2024-06-09 22:19:33 +0200

    Implemented PriorityQueue

--- sem4/aisd/l5/main.go
+++ sem4/aisd/l5/main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"math/rand"
 )
+
+type Item struct {
+	value    float64
+	priority int
+	index    int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  //Avoid memory leak
+	item.index = -1 // Negative index so you know something is wrong
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) update(item *Item, value float64, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
 
 const (
 	N = 10
@@ -17,14 +61,6 @@ type Graph struct {
 func newGraph(n int) *Graph {
 	matrix := createMatrix(n)
 	return &Graph{size: n, matrix: matrix}
-}
-
-func main() {
-	fmt.Println("Hello Graphs!")
-
-	graph := newRandomGraph(N)
-	printGraph(graph)
-
 }
 
 func createMatrix(n int) [][]float64 {
@@ -41,7 +77,6 @@ func newRandomGraph(n int) *Graph {
 		for j := i + 1; j < n; j++ {
 			weight := rand.Float64()
 			graph.matrix[i][j] = weight
-			weight = rand.Float64()
 			graph.matrix[j][i] = weight
 		}
 	}
@@ -57,4 +92,12 @@ func printGraph(g *Graph) {
 		}
 		fmt.Print("]\n")
 	}
+}
+
+func main() {
+	fmt.Println("Hello Graphs!")
+
+	graph := newRandomGraph(N)
+	printGraph(graph)
+
 }
