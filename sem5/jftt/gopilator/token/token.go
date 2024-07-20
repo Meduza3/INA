be71deebf55dfa13f5008696368fc8ae2ca52670
Meduza3 <jellyfish200325@gmail.com>
2024-07-20 23:29:49 +0200

    Added lexer

--- sem5/jftt/gopilator/token/token.go
+++ sem5/jftt/gopilator/token/token.go
@@ -3,59 +3,86 @@ package token
 type TokenType string
 
 type Token struct {
-	Type    TokenType
-	Literal string
+  Type    TokenType
+  Literal string
 }
 
 const (
-	ILLEGAL = "ILLEGAL"
-	EOF     = "EOF"
+  ILLEGAL = "ILLEGAL"
+  EOF     = "EOF"
 
-	PROCEDURE = "PROCEDURE"
-	IS        = "IS"
-	IN        = "IN"
-	END       = "END"
+  PROCEDURE = "PROCEDURE"
+  IS        = "IS"
+  IN        = "IN"
+  END       = "END"
 
-	PROGRAM = "PROGRAM"
+  PROGRAM = "PROGRAM"
 
-	ASSIGNMENT = ":="
-	SEMICOLON  = ";"
-	IF         = "IF"
-	THEN       = "THEN"
-	ELSE       = "ELSE"
-	ENDIF      = "ENDIF"
+  ASSIGNMENT = ":="
+  SEMICOLON  = ";"
+  IF         = "IF"
+  THEN       = "THEN"
+  ELSE       = "ELSE"
+  ENDIF      = "ENDIF"
 
-	WHILE    = "WHILE"
-	DO       = "DO"
-	ENDWHILE = "ENDWHILE"
+  WHILE    = "WHILE"
+  DO       = "DO"
+  ENDWHILE = "ENDWHILE"
 
-	REPEAT = "REPEAT"
-	UNTIL  = "UNTIL"
+  REPEAT = "REPEAT"
+  UNTIL  = "UNTIL"
 
-	READ  = "READ"
-	WRITE = "WRITE"
+  READ  = "READ"
+  WRITE = "WRITE"
 
-	PIDENTIFIER = "PIDENTIFIER"
-	NUM         = "NUM"
+  PIDENTIFIER = "PIDENTIFIER"
+  NUM         = "NUM"
 
-	LPARENT  = "["
-	RPARENT  = "]"
-	LBRACKET = "("
-	RBRACKET = ")"
-	COMMA    = ","
+  LPARENT  = "["
+  RPARENT  = "]"
+  LBRACKET = "("
+  RBRACKET = ")"
+  COMMA    = ","
 
-	T = "T"
+  T = "T"
 
-	PLUS    = "+"
-	MINUS   = "-"
-	MULT    = "*"
-	DIVIDE  = "/"
-	PERCENT = "%"
+  PLUS    = "+"
+  MINUS   = "-"
+  MULT    = "*"
+  DIVIDE  = "/"
+  PERCENT = "%"
 
-	EQUALS    = "="
-	NOTEQUALS = "!="
-	GREATER   = ">"
-	SMALLER   = "<"
-	GEQ       = ">="
-	LEQ       = "<="
+  EQUALS    = "="
+  NOTEQUALS = "!="
+  GREATER   = ">"
+  SMALLER   = "<"
+  GEQ       = ">="
+  LEQ       = "<="
 )
+
+var keywords = map[string]TokenType{
+  "PROCEDURE": PROCEDURE,
+  "IS": IS,
+  "IN": IN,
+  "END": END,
+  "PROGRAM": PROGRAM,
+  "IF": IF,
+  "THEN": THEN,
+  "ELSE": ELSE,
+  "ENDIF": ENDIF,
+  "WHILE": WHILE,
+  "DO": DO,
+  "ENDWHILE": ENDWHILE,
+  "REPEAT": REPEAT,
+  "UNTIL": UNTIL,
+  "READ": READ,
+  "WRITE": WRITE,
+  "T": T,
+}
+
+func LookupIdent(ident string) TokenType {
+  if tok, ok := keywords[ident]; ok {
+    return tok
+  }
+  return PIDENTIFIER
+}
